886036957a906f65ff7062d2b03db47614dbaf85
asbelljc <asbelljc@gmail.com>
2021-05-12 18:03:44 -0400

    UI almost finished; need to adjust button edges and transitions!

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ function divide(a, b) {
   return a / b;
 }
 
+function power(a, b) {
+  return a ** b;
+}
+
+function root(a, b) {
+  return a ** (1 / b);
+}
+
 function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
